ad0f7ba6235eb644e604e78f238ba47126cb8554
ned <155941270+ned123abc@users.noreply.github.com>
2024-04-26 11:11:50 +0800

    Update impl.go

--- cloudflare/impl/impl.go
+++ cloudflare/impl/impl.go
@@ -357,7 +357,7 @@ func UpdateFirewallRule(zoneID string) string {
         "rules": [
             {
                 "action": "block",
-                "expression": "(not ip.geoip.country in {"CN" "JP"})",
+                "expression": "(not ip.geoip.country in { \"CN\" \"JP\" })",
                 "description": "limit",
                 "enabled": true
             }
